7a3252a7cc253f4ddc8cbf3e0d5cff4098491c5d
ElpAlex <52038964+ElpAlex@users.noreply.github.com>
2021-08-31 09:16:27 +0100

    Update Response1.cs

--- Scripts/Response1.cs
+++ Scripts/Response1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+// attached to response choices that will have positive impact on th relationship with the other characters
 public class Response2 : MonoBehaviour
 {
     public Slider slider;
